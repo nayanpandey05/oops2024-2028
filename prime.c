0855e170102ebf7710a1da85b93a64ecb2f6aa32
nayanpandey05 <nayanpandey05@gmail.com>
2025-08-06 16:09:31 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,1 +1,29 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int num, i, Prime = 1;
+
+    printf("Enter a no:");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        Prime = 0; 
+    } 
+    else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                Prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (Prime)
+        printf("%d is a prime no.\n", num);
+    else
+        printf("%d is not a prime no.\n", num);
+
+    return 0;
+}
