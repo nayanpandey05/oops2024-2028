ca0fbffab745b9a59fde8ce676fb90e10cf8dc55
nayanpandey05 <nayanpandey05@gmail.com>
2025-08-29 19:20:20 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class addition {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a: ");
+        int a=sc.nextInt();
+        System.out.println("enter b: ");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("the sum is: "+ sum);
+    }
+    
+}
