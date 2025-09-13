fae1460ee24ec14ee6fc152ded8380a72fe1f5a2
nayanpandey05 <nayanpandey05@gmail.com>
2025-09-13 11:42:12 +0530

    Add files via upload

---
+++ thisdemo.java
@@ -0,0 +1,18 @@
+class ThisDemo {
+    int a,b;
+    public void get(int x,int y){
+        a=x;
+        b=y;
+    }
+    public void put(){
+        System.out.println("a ="+a);
+        System.out.println("b ="+b);
+    }
+}
+class Demo{
+    public static void main(String[] args) {
+        ThisDemo obj=new ThisDemo();
+        obj.get(10,20);
+        obj.put();
+    }
+}
